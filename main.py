43974fd913f11fa2b3b692a7fd33a0353bca9018
Loris <monestierloris11@gmail.com>
2022-11-07 16:41:33 +0100

    part2

--- main.py
+++ main.py
@@ -23,12 +23,16 @@ class ElementPile:
         self.elements:List[Element] = []
         self.size_max:int = size_max
         self.size:int = 0
+    def __repr__(self):
+        return f"ElementPile({self.elements})"
         
 class InstructionPile:
     def __init__(self, size_max:int, elements: List[Instruction]):
         self.elements:List[Instruction] = []
         self.size_max:int = size_max
         self.size:int = 0
+    def __repr__(self):
+        return f"InstructionPile({self.elements})"
 
 def add_ElementPile(pile:ElementPile, element:Element) -> None:
     if pile.size == pile.size_max:
@@ -52,16 +56,47 @@ def remove_InstructionPile(pile) -> Instruction:
     pile.size -= 1
     return pile.elements.pop()
 
+def instruction_from_data(data:str) -> Element:
+    liste_nombres:List[str] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    is_number:int = 1
+    for i in range(len(data)):
+        in_list:int = 0
+        for j in range(len(liste_nombres)):
+            if data[i] == liste_nombres[j]:
+                in_list = 1
+                break
+        if in_list == 0:
+            is_number = 0
+            break
+    if is_number:
+        return Instruction("push", Element(0, int(data), ""))
+    is_string:int = 1
+    if data[0] != "\"" or data[-1] != "\"":
+        is_string = 0
+    if is_string:
+        return Instruction("push", Element(1, 0, data[1:-1]))
+    return Instruction("cmd", Element(1, 0, data))
+
 def compile(code : str, liste_instructions : InstructionPile) -> None:
-    pass
+    code += " "
+    buffer : str = ""
+    for i in range(len(code)):
+        if code[i] == " ":
+            if buffer == "":
+                continue
+            add_InstructionPile(liste_instructions, instruction_from_data(buffer))
+            buffer = ""
+        else:
+            buffer += code[i]
 
 def run(liste_instructions : InstructionPile) -> None:
     pass
 
 def main():
-    code:str = "1 2 + ."
+    code:str = """1 2 + ."""
     liste_instructions:InstructionPile = InstructionPile(100, [])
     compile(code, liste_instructions)
+    print(liste_instructions)
     run(liste_instructions)
     
 main()
